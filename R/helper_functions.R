6a6aabb61d5b5d0af3e923c025a7518d70196379
Tom Auer <mta45@cornell.edu>
2017-08-30 15:55:57 -0400

    Added and documented a function for calculating the full spatial extent of positive data from a stack.

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -40,3 +40,79 @@ stack_stem <- function(path) {
 
   return(st)
 }
+
+#' Calculates spatial extent of a stack for plotting
+#'
+#' After creating a stack, there are lots of NA values and plots of the
+#' individual raster layers are at the full extent of the template_raster. To
+#' show an ideal extent, this function trims away 0 and NA values and checks
+#' to make sure it returns a reasonable extent (based on the template_raster)
+#' for plotting. The returned extent object can then be used for plotting.
+#'
+#' @usage \code{calc_full_extent(stack))}
+#'
+#' @param stack A RasterStack object, ideally of occurrence or abundace.
+#'
+#' @return raster Extent object
+#'
+#' @examples
+#' tif_path <- "~"
+#' raster_stack <- stack_stem(tif_path)
+#' plot_extent <- calc_full_extent(raster_stack)
+#' raster::plot(raster_stack[[1]], ext=plot_extent)
+calc_full_extent <- function(stack) {
+
+  # aggregate stack for speed, otherwise everything else takes too long
+  stack <- raster::aggregate(stack, fact=3)
+
+  # convert 0s to NAs, otherwise trimming is slow and the extent is too broad
+  stack[stack == 0] <- NA
+
+  # trim away 0s to get closest extent to positive values
+  stack <- raster::trim(stack, values = NA)
+
+  # save extent
+  map_extent <- raster::extent(stack)
+
+  # sometimes extent calculations get weird and you'll get a very broad
+  # extent that goes further than you want
+  # this section is intended to correct that by setting the mins and maxes
+  # to that of the template_raster
+  # however, if the stack comes in a different projection, we need to correct
+  # for that
+
+  # if stack projection is different from template_raster,
+  # project template_raster
+  if(raster::projection(template_raster) != raster::projection(stack)) {
+    this_template_raster <- projectRaster(template_raster,
+                                          raster::projection(stack))
+  } else {
+    this_template_raster <- template_raster
+  }
+
+  # create object of this template_raster for comparison to extent extracted
+  # from the stack above
+  template_raster_extent <- raster::extent(this_template_raster)
+
+  # xmin too low
+  if(map_extent[1] < template_raster_extent[1]) {
+    map_extent[1] <- template_raster_extent[1]
+  }
+
+  # xmax too high
+  if(map_extent[2] > template_raster_extent[2]) {
+    map_extent[2] <- template_raster_extent[2]
+  }
+
+  # ymin too low
+  if(map_extent[3] < template_raster_extent[3]) {
+    map_extent[3] <- template_raster_extent[3]
+  }
+
+  # ymax too high
+  if(map_extent[4] > template_raster_extent[4]) {
+    map_extent[4] <- template_raster_extent[4]
+  }
+
+  return(map_extent)
+}
