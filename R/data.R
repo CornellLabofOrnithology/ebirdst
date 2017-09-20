6e03cc62fa81034e93f7bac73e6655b98c18d95c
Tom Auer <mta45@cornell.edu>
2017-09-20 10:18:01 -0400

    Revising data documentation.

--- R/data.R
+++ R/data.R
@@ -12,18 +12,21 @@
 #' extent.
 #'
 #' @examples
-#' str(template_raster)
+#' str(stemhelper::template_raster)
 #'
 #' r <- raster()
-#' extended_raster <- extend(r, extent(template_raster))
+#' extended_raster <- extend(r, extent(stemhelper::template_raster))
 "template_raster"
 
 #' RasterStack of zero values based on checklist-level ensemble support
 #'
-#' A RasterStacks (one for each week of the year) indicating the ensemble support based on sufficient checklists to fit models within an individiual stixel. Values range from 0 to 100. For presentation, we use a value of 99 to indicate sufficient information to assume that, if there are no positive occurrence or abundance predictions within a given raster cell, that there is a value of 0 occurrence and 0 abundance.
+#' A RasterStack (one layer for each week of the year) indicating the ensemble support based on sufficient checklists to fit models within an individiual stixel. Values range from 0 to 100. For presentation, we use a value of 99 to indicate sufficient information to assume that, if there are no positive occurrence or abundance predictions within a given raster cell, that there is a value of 0 occurrence and 0 abundance.
 #'
 #' @format A RasterStack, where each layer is a week of the year and where the values range of 0 to 100 representing the number of contributing independent models (stixels) with sufficient checklists to fit and predict within that indepdendent model (stixel). Each RasterLayer in the RasterStack has the full spatial extent and same resolution and coordinate system of the STEM study extent.
 #'
 #' @examples
-#' zero_es_stack
+#' stemhelper::zero_es_stack
+#'
+#' # create binary layers for each week where to show assumed zeroes
+#' stemhelper::zero_es_stack[stemhelper::zero_es_stack < 99] <- NA
 "zero_es_stack"
