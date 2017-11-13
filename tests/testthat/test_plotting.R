1ec1d26983c48b4201a2a6e63fcf01388b057470
Tom Auer <mta45@cornell.edu>
2017-11-13 12:07:59 -0500

    Testing for plot_pds()

--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -136,4 +136,148 @@ test_that("stemhelper plot_pis", {
 })
 
 # plot_pds
+test_that("stemhelper plot_pds", {
+  root_path <- "~/Box Sync/Projects/2015_stem_hwf/documentation/data-raw/"
+  species <- "woothr-ERD2016-PROD-20170505-3f880822"
+  sp_path <- paste(root_path, species, sep = "")
+
+  pds <- load_pds(sp_path)
+
+  # expected with st_extent
+  ne_extent <- list(type = "rectangle",
+                    lat.min = 40,
+                    lat.max = 47,
+                    lon.min = -80,
+                    lon.max = -70,
+                    t.min = 0.425,
+                    t.max = 0.475)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent), NA)
+
+  # checking return quality
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent), "list")
+  expect_equal(length(plot_pds(pd_name = "EFFORT_HRS",
+                               pds = pds,
+                               st_extent = ne_extent)$pointwise), 3)
+  expect_equal(length(plot_pds(pd_name = "EFFORT_HRS",
+                               pds = pds,
+                               st_extent = ne_extent)$pointwise$t.median), 50)
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent)$pointwise, "list")
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent)$pointwise$t.median, "numeric")
+
+  # params
+  # quantiles
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        plot_quantiles = TRUE), NA)
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent,
+                     plot_quantiles = TRUE), "list")
+  expect_equal(length(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent,
+                     plot_quantiles = TRUE)), 2)
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent,
+                     plot_quantiles = TRUE)$quantiles, "list")
+  expect_equal(length(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent,
+                     plot_quantiles = TRUE)$quantiles), 2)
+  expect_is(plot_pds(pd_name = "EFFORT_HRS",
+                     pds = pds,
+                     st_extent = ne_extent,
+                     plot_quantiles = TRUE)$quantiles$t.ul, "numeric")
+  expect_equal(length(plot_pds(pd_name = "EFFORT_HRS",
+                               pds = pds,
+                               st_extent = ne_extent,
+                               plot_quantiles = TRUE)$quantiles$t.ul), 50)
+
+  # more params
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        plot_quantiles = TRUE,
+                        pointwise_pi = FALSE), NA)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        stixel_pds = TRUE), NA)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        ylim = 1), "invalid 'ylim' value")
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        print_plot = FALSE), NA)
+
+  # TODO add more tests for GBM parms
+
+  # nothing to do
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent,
+                        pointwise_pi = FALSE),
+               "Nothing to plot")
+
+  # missing temporal info
+  ne_extent <- list(type = "rectangle",
+                    lat.min = 40,
+                    lat.max = 47,
+                    lon.min = -80,
+                    lon.max = -70)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent),
+               "Must provide t.min and t.max")
+
+  # reversed min max
+  ne_extent <- list(type = "rectangle",
+                    lat.min = 47,
+                    lat.max = 40,
+                    lon.min = -80,
+                    lon.max = -70,
+                    t.min = 0.425,
+                    t.max = 0.475)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent),
+               "Latitude maximum is less than latitude minimum")
+
+  # missing a corner
+  ne_extent <- list(type = "rectangle",
+                    lat.min = 47,
+                    lat.max = 40,
+                    lon.min = -80,
+                    t.min = 0.425,
+                    t.max = 0.475)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent),
+               "Missing max longitude")
+
+  # st_extent is not list
+  ne_extent <- c(type = "rectangle",
+                 lat.min = 40,
+                 lat.max = 47,
+                 lon.min = -80,
+                 lon.max = -70,
+                 t.min = 0.425,
+                 t.max = 0.475)
+  expect_error(plot_pds(pd_name = "EFFORT_HRS",
+                        pds = pds,
+                        st_extent = ne_extent),
+               "st_extent argument must be a list")
+})
 # cake_plots
