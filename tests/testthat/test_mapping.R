e8b5ce2cd3464a7c26b3841a3fcabe4fddcda8c7
Matt Strimas-Mackey <mstrimas@gmail.com>
2019-03-28 10:21:38 -0400

    for tom

--- tests/testthat/test_mapping.R
+++ tests/testthat/test_mapping.R
@@ -1,5 +1,5 @@
 context("Mapping functions")
-
+skip_on_cran()
 sp_path <- download_data("example_data", tifs_only = FALSE)
 lp_extent <- ebirdst_extent(c(xmin = -86, xmax = -83, ymin = 42, ymax = 45),
                             t = c(0.5, 0.6))
