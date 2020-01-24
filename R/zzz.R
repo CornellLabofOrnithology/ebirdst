b7a9dfd9a929c471ae7be200c69ce6f96724448e
Matt Strimas-Mackey <mstrimas@gmail.com>
2020-01-24 12:59:43 -0500

    remove second link

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
   m <- paste("Please cite the eBird Status & Trends data using:",
              "Fink, D., T. Auer, A. Johnston, M. Strimas-Mackey, O. Robinson, S. Ligocki, B. Petersen, ",
              "C. Wood, I. Davies, B. Sullivan, M. Iliff, S. Kelling. 2019. eBird Status and Trends, ",
-             "Version: November 2019. https://ebird.org/science/status-and-trends. Cornell Lab of ",
-             "Ornithology, Ithaca, New York. https://doi.org/...",
+             "Version: November 2019. Cornell Lab of Ornithology, Ithaca, New York.",
+             "https://doi.org/...",
              sep = " \n  ")
   packageStartupMessage(m)
 }
