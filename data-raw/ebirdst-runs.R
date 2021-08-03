f5ec417f0c15cae107d676fc1d9e7805ac4607d2
Matt Strimas-Mackey <mes335@cornell.edu>
2021-08-03 14:07:21 -0400

    bring range_modeled into ebirdst_runs

--- data-raw/ebirdst-runs.R
+++ data-raw/ebirdst-runs.R
@@ -32,11 +32,15 @@ ebirdst_runs <- inner_join(runs, ebird_taxonomy, by = "species_code") %>%
   mutate_at(vars(ends_with("start")), ~ ymd(paste0("2019-", .))) %>%
   mutate_at(vars(ends_with("end")), ~ ymd(paste0("2019-", .))) %>%
   select(run_name, species_code, scientific_name, common_name, resident,
-         breeding_quality, breeding_start, breeding_end,
-         nonbreeding_quality, nonbreeding_start, nonbreeding_end,
+         breeding_quality, breeding_range_modeled,
+         breeding_start, breeding_end,
+         nonbreeding_quality, nonbreeding_range_modeled,
+         nonbreeding_start, nonbreeding_end,
          postbreeding_migration_quality,
+         postbreeding_migration_range_modeled,
          postbreeding_migration_start, postbreeding_migration_end,
          prebreeding_migration_quality,
+         prebreeding_migration_range_modeled,
          prebreeding_migration_start, prebreeding_migration_end,
          resident_quality, resident_start, resident_end)
 
